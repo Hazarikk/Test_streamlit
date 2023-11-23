929bcee34f06eb6d19b6fd15ba1a905ba1f67254
Hazarikk <86823920+Hazarikk@users.noreply.github.com>
2023-11-23 20:08:23 +0700

    Add files via upload

---
+++ MoviesProject/pages/loginpage.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+# from streamlit_option_menu import option_menu
+
+import testforlogin
+
+
+def __init__(self):
+        self.apps = []
+
+def add_app(self, title, func):
+
+        self.apps.append({
+            "title": title,
+            "function": func
+        })
+        
+
+def run():
+ testforlogin.app()    
+             
+          
+run()        
